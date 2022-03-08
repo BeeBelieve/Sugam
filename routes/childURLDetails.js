6a60d5c44d78d6a9bea36294ef8e99e2b6c308f1
Priya <priya162125@gmail.com>
2022-03-08 22:38:43 +0530

    Added try and catch

--- routes/childURLDetails.js
+++ routes/childURLDetails.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 var express = require("express");
 var router = express.Router();
 var fs = require("fs");
@@ -14,32 +15,37 @@ router.get("/", function (req, res, next) {
 		"'";
 	db.query(sql, function (err, results) {
 		if (results.length) {
-			var data_array = {};
-			for (var j = 0; j < results.length; j++) {
-				if (results[j]["web_id"] == web_id) {
-					var intI = j;
+			try {
+				var data_array = {};
+				for (var j = 0; j < results.length; j++) {
+					if (results[j]["web_id"] == web_id) {
+						var intI = j;
 
-					fs = require("fs");
-					var getName = results[intI]["webname"];
+						fs = require("fs");
+						var getName = results[intI]["webname"];
 
-					if (getName == "") {
-						getName = "< Missing title >" + results[k].pageUrl;
+						if (getName == "") {
+							getName = "< Missing title >" + results[k].pageUrl;
+						}
+
+						var newStringName = getName.replace(/[^A-Z0-9]/gi, "_");
+
+						var filename = md5(newStringName) + ".json";
+						//var folderName = results[0]["url"].split("/")[2];
+						var folderName = results[intI]["folder"];
+						var filepath =
+							//  __dirname +
+							"public/json/" + folderName + "/" + filename;
+						let rawdata = fs.readFileSync(filepath);
+						let errors = JSON.parse(rawdata);
+						data_array[0] = results;
+						data_array[1] = errors.issues;
+						res.render("resultDetails.ejs", { data: data_array });
 					}
-
-					var newStringName = getName.replace(/[^A-Z0-9]/gi, "_");
-
-					var filename = md5(newStringName) + ".json";
-					//var folderName = results[0]["url"].split("/")[2];
-					var folderName = results[intI]["folder"];
-					var filepath =
-						//  __dirname +
-						"public/json/" + folderName + "/" + filename;
-					let rawdata = fs.readFileSync(filepath);
-					let errors = JSON.parse(rawdata);
-					data_array[0] = results;
-					data_array[1] = errors.issues;
-					res.render("resultDetails.ejs", { data: data_array });
 				}
+			} catch (error) {
+				console.log("Something went wrong.");
+				res.redirect("resultDetails?scan_id=" + scan_id);
 			}
 		} else {
 			message = "No Record Founds.";
